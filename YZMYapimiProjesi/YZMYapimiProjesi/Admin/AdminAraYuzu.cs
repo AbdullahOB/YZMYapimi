eee78a61928605ec9a1147956656fdea3ec3f9d9
AbdullahOB <abdullahobeid2@gmail.com>
2021-05-06 00:47:51 +0300

    Admin Page + Access DB

---
+++ YZMYapimiProjesi/YZMYapimiProjesi/Admin/AdminAraYuzu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YZMYapimiProjesi.Admin
+{
+    public partial class AdminAraYuzu : Form
+    {
+        public AdminAraYuzu()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminAraYuzu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void geriDonButonu(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
