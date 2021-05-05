4fde3c3b36ba94a9629503429e60e1dd0a88e42b
AbdullahOB <abdullahobeid2@gmail.com>
2021-05-06 02:00:36 +0300

    Work On Admin Page

--- YZMYapimiProjesi/YZMYapimiProjesi/Admin/backgroundStyle.cs
+++ YZMYapimiProjesi/YZMYapimiProjesi/Admin/backgroundStyle.cs
@@ -15,16 +15,18 @@ namespace YZMYapimiProjesi.Admin
 
         public Color topColor { get; set; }
         public Color bottomColor { get; set; }
-
+        
         public float Angle { get; set; }
 
+        
         protected override void OnPaint(PaintEventArgs e)
         {
+            
             LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, this.topColor, this.bottomColor, this.Angle);
-
+            
             Graphics g = e.Graphics;
 
-            Label L = new Label();
+            
 
             g.FillRectangle(brush, this.ClientRectangle);
 
