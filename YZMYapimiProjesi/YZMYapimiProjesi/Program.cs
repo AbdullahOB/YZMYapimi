8ab861a46af4739a67a3ac45cfb5db48678295d3
Abdullah Obaid <abdullahobeid2@gmail.com>
2021-05-08 16:02:31 +0300

    FromAbdullah

--- YZMYapimiProjesi/YZMYapimiProjesi/Program.cs
+++ YZMYapimiProjesi/YZMYapimiProjesi/Program.cs
@@ -16,7 +16,7 @@ namespace YZMYapimiProjesi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new Admin.AdminAraYuzu());
         }
     }
 }
