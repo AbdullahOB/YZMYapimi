b0f70f39d7fcb3e13ce35257c29edca7e6274fbc
Abdullah Obaid <abdullahobeid2@gmail.com>
2021-06-21 11:24:08 +0300

    d

--- YZMYapimiProjesi/YZMYapimiProjesi/DB/DbModelYZM.Designer.cs
+++ YZMYapimiProjesi/YZMYapimiProjesi/DB/DbModelYZM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Abdullah\OneDrive\Desktop\Yazilim Yapimi\YZMYapimi(Done)\YZMYapimi\YZMYapimiProjesi\YZMYapimiProjesi\DB\DbModelYZM.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Abdullah\OneDrive\Desktop\YZMYapimi\YZMYapimiProjesi\YZMYapimiProjesi\DB\DbModelYZM.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
